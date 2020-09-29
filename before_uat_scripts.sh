a16af074b263917002ed2bcc32f20269ed125173
Divya Bhargavi Yakkala <divya.yakkala@valuelabs.com>
2020-09-29 18:30:15 +0530

    aa

--- before_uat_scripts.sh
+++ before_uat_scripts.sh
@@ -3,8 +3,8 @@ echo $RELEASE_VERSION
 echo 'test string'
 if [ ${TRAVIS_PULL_REQUEST_BRANCH} = "test-branch" ]; then
     echo "IFBLOCK"
-    if [ grep -q Prod test.txt ]; then
-        echo "2nd IF"
+if [ grep -q Prod test.txt ]; then
+    echo "2nd IF"
 fi
 set |grep TRAVIS
 npm run test --coverage
