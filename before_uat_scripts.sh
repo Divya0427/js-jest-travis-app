de034c5deff31cc2ed576797b9ca437b664cdde4
Divya Bhargavi Yakkala <divya.yakkala@valuelabs.com>
2020-09-29 19:57:12 +0530

    dd

--- before_uat_scripts.sh
+++ before_uat_scripts.sh
@@ -1,11 +1,13 @@
 echo $TRAVIS_PULL_REQUEST_BRANCH
 echo $RELEASE_VERSION
 echo 'test string'
-if [ ${TRAVIS_PULL_REQUEST_BRANCH} = "test-branch" ]; then
+if [ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" ]; then
     echo "IFBLOCK"
 fi
-if [ grep -q Prod test.txt ]; then
-    echo "2nd IF"
+if [ ${TRAVIS_BRANCH} == "develop" ]; then
+    sed -i 's/Prod/UAT/${RELEASE_VERSION}' test.txt || travis_terminate 1
+else
+    echo 'NOT DEVELOP'
 fi
 set |grep TRAVIS
 npm run test --coverage
