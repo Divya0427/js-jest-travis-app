9603a483b582b0b2de19b663a8ef88c1c23de9f6
Divya Bhargavi Yakkala <divya.yakkala@valuelabs.com>
2020-09-29 21:16:50 +0530

    added new global variables

--- before_uat_scripts.sh
+++ before_uat_scripts.sh
@@ -1,9 +1,10 @@
 echo $TRAVIS_PULL_REQUEST_BRANCH
 echo $RELEASE_VERSION
+echo $PROD_URL
+echo $UAT_URL
 echo 'test string'
 if [ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" ]; then
-    echo "IFBLOCK"
-    sed -i 'e/UAT/Prod/${RELEASE_VERSION}' test.txt || travis_terminate 1
+    sed -i 's/UAT/${PROD_URL}' test.txt || travis_terminate 1
 fi
 if [ ${TRAVIS_BRANCH} == "develop" ]; then
     sed -i 's/Prod/UAT/${RELEASE_VERSION}' test.txt || travis_terminate 1
