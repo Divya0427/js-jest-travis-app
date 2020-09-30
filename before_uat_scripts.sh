9298c97e329aba81b92bcd326b007b903d2c6c43
Divya Bhargavi Yakkala <divya.yakkala@valuelabs.com>
2020-09-30 16:42:22 +0530

    D

--- before_uat_scripts.sh
+++ before_uat_scripts.sh
@@ -6,17 +6,13 @@ echo 'BEFORE UAT SCRIPTS'
 if [ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" ]; then
     cat test.txt
     sed -i.bak 's|https://google.com|https://localhost:7777|g' test.txt
+    sed -i.bak 's|${DEFAULT_LOCALHOST}|${UAT_URL}|g' test.txt
     cat test.txt
 fi
 if [ ${TRAVIS_BRANCH} == "develop" ]; then
     sed -i 's/Prod/UAT/${RELEASE_VERSION}' test.txt
 else
     echo 'NOT DEVELOP'
-fi
-if [ ${TRAVIS_BRANCH} == "master" ]; then
-    sed -i 's/UAT/Prod/${RELEASE_VERSION}' test.txt
-else
-    echo 'NOT MASTER'
 fi
 set |grep TRAVIS
 npm run test --coverage
