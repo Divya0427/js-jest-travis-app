d1a3a053a9c47940893faa9f61cabbb7babe794f
Divya Bhargavi Yakkala <divya.yakkala@valuelabs.com>
2020-09-30 10:25:11 +0530

    kk

--- before_uat_scripts.sh
+++ before_uat_scripts.sh
@@ -4,6 +4,7 @@ echo $PROD_URL
 echo $UAT_URL
 echo 'test string'
 if [ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" ]; then
+    perl -pi -e 's/UAT/Prod/g' test.txt
     sed -i 's@UAT@${PROD_URL}' test.txt
     sed -i 's|UAT|${PROD_URL}' test.txt
     sed "s/foo/${PROD_URL}/g" test.txt
@@ -18,9 +19,6 @@ if [ ${TRAVIS_BRANCH} == "master" ]; then
 else
     echo 'NOT MASTER'
 fi
-sed -i 's@UAT@${PROD_URL}' test.txt
-sed -i 's|UAT|${PROD_URL}' test.txt
-sed "s/foo/${PROD_URL}/g" test.txt
 set |grep TRAVIS
 npm run test --coverage
 npm run build
