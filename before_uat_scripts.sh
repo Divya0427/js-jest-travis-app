ec9d0096afab731239aec7351768b94b06c51275
Divya Bhargavi Yakkala <divya.yakkala@valuelabs.com>
2020-09-30 13:31:49 +0530

    ooo

--- before_uat_scripts.sh
+++ before_uat_scripts.sh
@@ -4,10 +4,12 @@ echo $PROD_URL
 echo $UAT_URL
 echo 'test string'
 if [ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" ]; then
+
     perl -pi -e 's/UAT/Prod/g' test.txt
     sed -i 's@UAT@${PROD_URL}' test.txt
     sed -i 's|UAT|${PROD_URL}' test.txt
     sed "s/foo/${PROD_URL}/g" test.txt
+    cat test.txt
 fi
 if [ ${TRAVIS_BRANCH} == "develop" ]; then
     sed -i 's/Prod/UAT/${RELEASE_VERSION}' test.txt
