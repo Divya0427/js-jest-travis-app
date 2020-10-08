884e02a32f6497191e94f7dc00529b3345e0f139
Divya Bhargavi Yakkala <divya.yakkala@valuelabs.com>
2020-10-08 15:28:01 +0530

    mnm

--- before_uat_scripts.sh
+++ before_uat_scripts.sh
@@ -19,12 +19,6 @@ if [ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" ]; then
     cd $RELEASE_VERSION
     ls -a
 fi
-if [[ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" AND $TRAVIS_PULL_REQUEST == 1 ]]; then
-    echo 'Its not merge'
-fi
-if [[ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" AND $TRAVIS_PULL_REQUEST != 1 ]]; then
-    echo 'Its a merge'
-fi
 if [[ ${TRAVIS_PULL_REQUEST_BRANCH} == "test-branch" && $TRAVIS_PULL_REQUEST == 1 ]]; then
     echo '&Its not merge'
 fi
